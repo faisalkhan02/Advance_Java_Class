13f660cd2d6aa61d96b319dc435e60e6731b6636
Faisal Khan <fs786fs786@gmail.com>
2026-02-10 09:56:55 +0530

    Add program inside class after removed

--- src/Unit_1/A05_DefaultMethod.java
+++ src/Unit_1/A05_DefaultMethod.java
@@ -1,5 +1,36 @@
 package Unit_1;
 
-public class A05_DefaultMethod {
 
+interface inter1{   //default method ke liye
+
+    default void def1() {
+        System.out.println("Default method inside the interface1");
+    }
+}
+interface inter3{   //static method ke liye
+    static int add(int x,int y) {
+        return x+y;
+    }
+}
+interface inter2{   //default method ka hi hai
+
+    default void def1() {
+        System.out.println("Default method inside the interface2");
+    }
+}
+public class A05_DefaultMethod implements inter1,inter2,inter3 {
+
+    public void def1() {
+        inter1.super.def1();
+        inter2.super.def1();
+    }
+    public static void main(String[] args) {
+        A05_DefaultMethod d=new A05_DefaultMethod();
+        d.def1();
+        System.out.println(inter3.add(4, 5)); //yaha pe static method ko call kiya
+
+
+
+
+    }
 }
