a8197a3ec6f4706e1d0f88ef80a4581d3ffc315d
Faisal Khan <fs786fs786@gmail.com>
2026-02-10 21:25:30 +0530

    class work

---
+++ src/Unit_1/A08_Var.java
@@ -0,0 +1,24 @@
+package Unit_1;
+
+class Test2{
+    Test2(){
+        var x =23;
+        System.out.println("constructor value "+x);
+    }
+    void check(){
+        var s ="hello";
+//        return s;
+        System.out.println(s);
+
+    }
+}
+public class A08_Var {
+    public static void main(String[] args) {
+        Test2 t = new Test2();
+        t.check();
+
+        for(var i =0;i<=4;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Unit_1/A09_TextBlock.java
@@ -0,0 +1,19 @@
+package Unit_1;
+
+public class A09_TextBlock {
+    public static void main(String[] args) {
+
+        // if we want to write text in multiple line we uses text block
+        // syntax:
+        // String text = """
+        //`                 content
+        //               """;
+        String s = """      
+                    11/2,
+                    KIET,Muradnagar,
+                    Ghaziabad
+                
+                """;
+        System.out.println(s);
+    }
+}
