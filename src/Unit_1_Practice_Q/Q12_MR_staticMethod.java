15ad0b9b4508b0feadb94dda0c32f5723d82e1fa
Faisal Khan <fs786fs786@gmail.com>
2026-02-12 14:36:21 +0530

    Practice Questions of class

---
+++ src/Unit_1_Practice_Q/Q12_MR_staticMethod.java
@@ -0,0 +1,39 @@
+package Unit_1_Practice_Q;
+// Create a interface MathUtil with static methods and use method reference to:
+// add numbers
+// find maximum
+// check even/odd
+
+
+interface addOperate{
+    int operate(int a,int b);
+}
+interface maxOperate{
+    int findMax(int a,int b);
+}
+interface evenOdd{
+    boolean isEven(int a);
+}
+
+interface MathUtil{
+    static int add(int x,int y){
+        return x+y;
+    }
+    static int maximum(int x,int y){
+        return (x>y)?x:y;
+    }
+    static boolean evenOddCheck(int x){
+        return (x % 2 == 0);
+    }
+}
+public class Q12_MR_staticMethod {
+    public static void main(String[] args) {
+        addOperate a1 = MathUtil::add;
+        maxOperate max = MathUtil::maximum;
+        evenOdd check1 = MathUtil::evenOddCheck;
+
+        System.out.println("addition: "+a1.operate(5,3));
+        System.out.println("maximum: "+max.findMax(5,8));
+        System.out.println("Is even: "+check1.isEven(7));
+    }
+}
---
+++ src/Unit_1_Practice_Q/Q13_forEach_map.java
@@ -0,0 +1,32 @@
+package Unit_1_Practice_Q;
+import java.util.HashMap;
+import java.util.Map;
+
+// Use forEach() on a Map<Integer,String>to:
+// print key and values
+// print only keys greater than 100
+
+
+
+
+public class Q13_forEach_map {
+    public static void main(String[] args) {
+        Map<Integer,String> map1 =new HashMap <>();
+        map1.put(112,"Faisal Khan");
+        map1.put(41,"Gaurav");
+        map1.put(55,"Hemant");
+        map1.put(50,"Deepak");
+        map1.put(150,"Devansh");
+        map1.put(250,"Talib");
+        map1.put(530,"AAtir");
+        map1.put(350,"Aman");
+        map1.put(30,"None");
+
+        map1.forEach((key,value) -> System.out.println("key: "+key+"  value: "+value));
+
+        System.out.println("\n=== key greater than 100 ===");
+        map1.forEach((key,value)->{
+            if(key>100) System.out.println(key);
+        });
+    }
+}
